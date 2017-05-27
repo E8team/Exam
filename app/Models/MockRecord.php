46140a284b0e3a537a1b6e8265e98afad3e772d2
wqer <2013855675@qq.com>
2017-05-27 20:32:55 +0800

    获取用户模拟时的提交记录

--- app/Models/MockRecord.php
+++ app/Models/MockRecord.php
@@ -6,5 +6,12 @@ namespace App\Models;
 
 class MockRecord extends BaseModel
 {
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 
+    public function submitRecord(){
+        return $this->hasMany(SubmitRecord::class);
+    }
 }

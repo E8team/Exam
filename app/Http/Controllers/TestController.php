b08fb87082135d1588cda42bbbdd9bbd71c25fe8
SayHe110 <949426374@qq.com>
2017-05-28 02:14:56 +0800

    填充submit_records模拟用户提交

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,6 +9,13 @@ class TestController
 
     public function courses()
     {
-
+        $a = app(TopicService::class)->findTopic(20)->toArray();
+        //dd($a);
+        $selOptionId = random_int(0,3);
+        if($a['options'][$selOptionId]['is_correct'] == 1){
+            echo 'success';
+        }else{
+            echo 'error';
+        }
     }
 }

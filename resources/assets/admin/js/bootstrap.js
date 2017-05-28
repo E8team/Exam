8db3af151dc6eadf0cb800a4006b9aee5ab2928a
tw <t888y@qq.com>
2017-05-28 13:16:37 +0800

    set import statement to top

--- resources/assets/admin/js/bootstrap.js
+++ resources/assets/admin/js/bootstrap.js
@@ -1,9 +1,6 @@
-try {
-    window.$ = window.jQuery = require('jquery');
-    require('bootstrap-less');
-    //import 'admin-lte/dist/css/AdminLTE.css';
-    //import 'admin-lte/dist/css/skins/skin-blue.css';
-    import 'admin-lte/bootstrap/js/bootstrap.js';
-    //import 'admin-lte/dist/js/app.js';
-
-} catch (e) {}
+import 'admin-lte/dist/css/AdminLTE.css';
+import 'admin-lte/dist/css/skins/skin-blue.css';
+import 'admin-lte/bootstrap/js/bootstrap.js';
+import 'admin-lte/dist/js/app.js';
+window.$ = window.jQuery = require('jquery');
+require('bootstrap-less');

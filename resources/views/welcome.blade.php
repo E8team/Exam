199a5a6c9f94d741a2c98b15b5fb98c6a64b63b8
tianyang <1506125048@qq.com>
2017-06-04 20:00:16 +0800

    添加进度条

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,8 @@
 <div class="up_top_padding bg_box">
 	<h2>马克思学院在线练习系统</h2>
 	<h3>在这里你可以了解中华上下五千年</h3>
-	<a href="{{route('register')}}" type="button" class="btn btn-info btn-lg">注册一个学生账号</a>
-	<div class="chevron-down"><i class="glyphicon glyphicon-chevron-down"></i></div>
+	<a href="{{route('register')}}" type="button" class="btn btn-info btn-lg hidden-xs hidden-md">注册一个学生账号</a>
+	<div class="chevron-down hidden-xs hidden-md"><i class="glyphicon glyphicon-chevron-down"></i></div>
 </div>
 <!-- 选课 -->
 <div class="container course">

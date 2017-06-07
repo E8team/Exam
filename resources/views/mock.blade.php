5fb115a3936bbf987b9de150fbaa4b5e42e569bb
tianyang <1506125048@qq.com>
2017-06-07 16:48:31 +0800

    添加题目通过率

--- resources/views/mock.blade.php
+++ resources/views/mock.blade.php
@@ -275,7 +275,10 @@
         <ul class="exam_list">
           @foreach($topics as $k => $topic)
             <li @if(!$topic->submitRecords->isEmpty())answered="true"@endif class="exam_item" data-id="topic_{!! $topic->id !!}" data-topic-id="{!! $topic->id !!}">
-              <p class="subject"><span>{!! $k+1 !!}</span> . {!! $topic->title !!}
+              <p class="subject">
+                <span class="num">{!! $k+1 !!}</span>
+                <span class="passing_rate">[通过率: 40.5%]</span>
+                {!! $topic->title !!}
                 <button type="button" class="view_ans btn-sm btn btn-link @if(!$topic->submitRecords->isEmpty() && !$topic->submitRecords->first()->is_correct) show" data-ans="{!! $topic->getAns() !!}" @else " @endif>查看答案</button>
               </p>
               <ul class="option_list">

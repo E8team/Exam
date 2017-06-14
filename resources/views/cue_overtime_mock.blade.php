244f9fd2c4948d8d15957fbb822fb746e69e121e
taoyu <1255803549@qq.com>
2017-06-14 14:52:22 +0800

    up

--- resources/views/cue_overtime_mock.blade.php
+++ resources/views/cue_overtime_mock.blade.php
@@ -4,8 +4,9 @@
   <div class="container prompt_box">
     <form method="post" action="{!! route('end_mock') !!}">
       <p class="content">您上次的"随机答题"已经结束</p>
+      <p>开始答题时间：{!! $mockRecord->created_at->toDateTimeString() !!}</p>
+      <p>结束答题时间：{!! $mockRecord->ended_at->toDateTimeString() !!}</p>
       <div class="prompt_btn">
-        <a  type="button" class="btn btn-success">继续答题</a>
           {!! csrf_field() !!}
           <input type="hidden" name="mock_record_id" value="{!! $mockRecord->id !!}">
           <button  type="submit" class="btn btn-primary">交卷</button>
